4455a3fdf07d3eb1b5014a9bfea98828a6065eec
Zach Anderson <zach@zabot.dev>
2023-08-24 19:59:08 -0500

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    flake-utils.follows = "cargo2nix/flake-utils";
+    nixpkgs.follows = "cargo2nix/nixpkgs";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, cargo2nix, rust-overlay, ... }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        overlays = [ cargo2nix.overlays.default rust-overlay.overlays.default ];
+      };
+
+      rustPkgs = pkgs.rustBuilder.makePackageSet {
+        rustVersion = "1.63.0";
+        packageFun = import ./Cargo.nix;
+      };
+    in
+    {
+      packages.x86_64-linux = rec {
+        fel = (rustPkgs.workspace.fel { });
+      };
+
+      devShells."x86_64-linux".default = (rustPkgs.workspaceShell {
+        nativeBuildInputs = [
+          cargo2nix.packages.x86_64-linux.default
+          pkgs.rustfmt
+          pkgs.go-containerregistry
+        ];
+      });
+    };
+}
